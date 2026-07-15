88353c06686118a84e5906e4e196bfaaf4d313f2
veenasuragani <suragani.veena@gmail.com>
2026-07-15 17:16:07 -0500

    commit

--- tests/javascriptcoding/SDET/reverseSentence.js
+++ tests/javascriptcoding/SDET/reverseSentence.js
@@ -2,5 +2,10 @@ function reverseWordOrder(sentence) {
   return sentence.split(' ').reverse().join(' ');
 }
 
+function reversesentence(sentence){
+  const words = sentence.split(' ');
+    return words.reduceRight((acc, word) => acc + ' ' + word, '').trim();
+}
+
 console.log(reverseWordOrder("I love JavaScript"));
 // JavaScript love I
--- tests/javascriptcoding/palindrome.js
+++ tests/javascriptcoding/palindrome.js
@@ -7,9 +7,17 @@ console.log(palindrome('A man, a plan, a canal, Panama')); // true
 console.log(palindrome('No lemon, no melon')); // true
 console.log(palindrome('Hello World')); //false 
 
+//without using built-in methods
+function palindrome2(str){
+    const clean = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    let reversed = [...clean].reduceRight((acc, c) => acc+c, '');
+    return clean === reversed;
+}
+
 
 /*
 "hello".split('')      // ['h', 'e', 'l', 'l', 'o']
 .reverse()             // ['o', 'l', 'l', 'e', 'h']
 .join('')              // "olleh"
 */
+console.log(palindrome2('A man, a plan, a canal, Panama')); // true
